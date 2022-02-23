2308ce5f7bdd3891c640d68e92c64f4a6cf7ddce
Matthew Ma <mtthman@amazon.com>
2022-02-23 00:57:07 -0800

    Refactor constants

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { SiteLocations } from './pages/contact-us/SiteLocations';
 import { Home } from './pages/home/Home';
 import { ProgramContacts } from './pages/contact-us/ProgramContacts';
+import { HOME, PROGRAM_CONTACTS, SITE_LOCATIONS } from './pages/constants';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to CISC Edmonds!</h1>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route path="contact-us/site-locations" element={<SiteLocations />} />
-        <Route path="contact-us/program-contacts" element={<ProgramContacts />} />
+        <Route path={HOME} element={<Home />} />
+        <Route path={SITE_LOCATIONS} element={<SiteLocations />} />
+        <Route path={PROGRAM_CONTACTS} element={<ProgramContacts />} />
       </Routes>
     </div>
   );
--- src/pages/constants.tsx
+++ src/pages/constants.tsx
@@ -1,30 +1,53 @@
+export const HOME = "/";
+
+export const ABOUT_US = "/about-us/";
+export const BOARD_OF_DIRECTORS = "/about-us/board-of-directors/";
+export const CAREERS = "/about-us/careers/";
+export const ANNUAL_REPORTS = "/about-us/annual-reports/";
+export const FAQS = "/about-us/faqs";
+
+export const EARLY_CHILDHOOD_EDUCATION = "/programs/early-childhood-education";
+export const YOUTH_DEVELOPMENT = "/programs/youth-development";
+export const FAMILY_SUPPORT = "/programs/family-support/";
+export const SENIOR_AND_DISABLED_ADULT_SERVICES = "/programs/senior-and-disabled-adult-services";
+export const HEALTHCARE_ACCESS = "/programs/healthcare-access";
+export const CIVIC_ENGAGEMENT = "/programs/civic-engagement";
+
+export const VOLUNTEER = "/get-involved/volunteer";
+export const IN_KIND_DONATION = "/get-involved/in-kind-donation";
+
+export const RESOURCES = "/resources/";
+
+export const SITE_LOCATIONS = "/contact-us/site-locations";
+export const PROGRAM_CONTACTS = "/contact-us/program-contacts";
+
 export const SITE_DIR: Record<string, any> = {
-  "HOME": "/",
+  "HOME": HOME,
   "ABOUT US": {
-    "About Us": "/about-us/",
-    "Leadership": "/about-us/board-of-directors/",
-    "Career Opportunities": "/about-us/careers/",
-    "Annual Reports & Newsletters": "/about-us/annual-reports/",
-    "FAQs": "/about-us/faqs"
+    "About Us": ABOUT_US,
+    "Leadership": BOARD_OF_DIRECTORS,
+    "Career Opportunities": CAREERS,
+    "Annual Reports & Newsletters": ANNUAL_REPORTS,
+    "FAQs": FAQS
   },
   "PROGRAMS": {
-    "Early Childhood Education": "/programs/early-childhood-education",
-    "Youth Development": "/programs/youth-development",
-    "Family Support": "/programs/family-support/",
-    "Senior and Disabled Adult Services": "/programs/senior-and-disabled-adult-services",
-    "Healthcare Access": "/programs/healthcare-access",
-    "Civic Engagement": "/programs/civic-engagement",
+    "Early Childhood Education": EARLY_CHILDHOOD_EDUCATION,
+    "Youth Development": YOUTH_DEVELOPMENT,
+    "Family Support": FAMILY_SUPPORT,
+    "Senior and Disabled Adult Services": SENIOR_AND_DISABLED_ADULT_SERVICES,
+    "Healthcare Access": HEALTHCARE_ACCESS,
+    "Civic Engagement": CIVIC_ENGAGEMENT,
   },
   "GET INVOLVED": {
     "Donation": "/",
     "Friendship Dinner": "/",
-    "Volunteer": "/get-involved/volunteer",
-    "In-Kind & Stock Donations": "/get-involved/in-kind-donation"
+    "Volunteer": VOLUNTEER,
+    "In-Kind & Stock Donations": IN_KIND_DONATION
   },
-  "RESOURCES": "/resources/",
+  "RESOURCES": RESOURCES,
   "CONTACT US": {
-    "Site Locations": "/contact-us/site-locations",
-    "Program Contacts": "/contact-us/program-contacts",
+    "Site Locations": SITE_LOCATIONS,
+    "Program Contacts": PROGRAM_CONTACTS,
     "Join our Newsletter": "/"
   }
-}
+};
